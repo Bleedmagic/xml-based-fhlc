ee206086ae8b2109a83e95a3a68b9444cab92b6b
nicoleelliena7 <pizarro_ellienanicole@plpasig.edu.ph>
2025-05-26 21:34:43 +0800

    ok

--- src/frontend/auth/logout.php
+++ src/frontend/auth/logout.php
@@ -28,5 +28,6 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Expires: 0");
 header("Pragma: no-cache");
 
-header("Location: ../home.php");
+header("Location: ../auth/login.php");
 exit;
+
